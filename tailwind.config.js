42de39d1cfe6c16e11c1999cad85490f6dd77c70
bhumitaylor14 <141008551+bhumitaylor14@users.noreply.github.com>
2023-12-22 11:40:26 +0530

    Removed unnecessary content from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 {import('tailwindcss').Config}
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./node_modules/YOUR_LIBRARY/**/*.{js,ts,jsx,tsx}"], 
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], 
   theme: {
     extend: {},
   },
