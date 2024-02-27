6fab592170594027d7328f95c0fd2017e32fd999
bhumitaylor14 <141008551+bhumitaylor14@users.noreply.github.com>
2024-02-27 14:53:59 +0530

    Update Test cases

--- __tests__/SendNativeToken.test.tsx
+++ __tests__/SendNativeToken.test.tsx
@@ -1,58 +1,69 @@
-import {render, fireEvent, cleanup } from '@testing-library/react';
-import SendNativeToken from '../src/components/SendNativeToken/index';
+/* MIT License
+ *
+ * Copyright (c) 2024 Etherspot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import React from 'react';
+import { ethers } from 'ethers';
+import { render, fireEvent, cleanup, getByRole, getByTestId } from '@testing-library/react';
+import SendNativeToken from '../src/components/SendNativeToken';
 
 afterEach(cleanup);
+
+const ethersProvider = new ethers.providers.JsonRpcProvider('http://localhost:3000', 'goerli'); // replace with your node's RPC URL
+const provider = ethers.Wallet.createRandom().connect(ethersProvider);
+
 test('Send native token renders correctly', () => {
-  const tree = render(<SendNativeToken
-    receiverAddress="0x725404c8Eead111d9E6DFE118c535F43402a9511"
-    chain={1} 
-    className= 'block w-1/2 rounded-md border-1 border-gray-300 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-    unstyled={false} 
-    debug={true}
-    disableSendOnEnter={false}
-    onlyEstimate={false}
-  />);
+  const tree = render(
+    <SendNativeToken
+      disableSendOnEnter
+      chainId={1}
+      provider={provider}
+      value={'0.1'}
+      onChangeValue={() => {}}
+      decimals={9}
+     receiverAddress={'0x0493b9a21dE42546B2E3687Da683D0B7B6ec2180'}
+      className="block rounded-md border-0 px-3.5 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    />,
+  );
   expect(tree).toMatchSnapshot();
 });
 
 test('input value updates correctly', () => {
   // Arrange
-  const { getByPlaceholderText } = render(<SendNativeToken
-    receiverAddress="0x725404c8Eead111d9E6DFE118c535F43402a9511"
-    chain={1} 
-    className= 'block w-1/2 rounded-md border-1 border-gray-300 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-    unstyled={false} 
-    debug={true}
-    disableSendOnEnter={false}
-    onlyEstimate={false}
-  />
+  const { getByPlaceholderText } = render(
+    <SendNativeToken
+      disableSendOnEnter
+      chainId={1}
+      provider={provider}
+      onChangeValue={() => {}}
+      decimals={9}
+      value={'0.1'}
+      receiverAddress={'0x0493b9a21dE42546B2E3687Da683D0B7B6ec2180'}
+      className="block rounded-md border-0 px-3.5 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    />,
   );
 
-  // Act: Change the input value
-  const inputElement = getByPlaceholderText('Enter Amount');
-  fireEvent.change(inputElement, { target: { value: '0.001' } });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const inputElement: any = getByPlaceholderText('0.00');
+  fireEvent.change(inputElement, { target: { value: '0.123' } });
 
-  // Assert: Check if the input value has been updated
-  expect(inputElement.value).toBe('0.001');
-});
-
-test('input value updates correctly', () => {
-  // Arrange
-  const { getByPlaceholderText } = render(<SendNativeToken
-    receiverAddress="0x725404c8Eead111d9E6DFE118c535F43402a9511"
-    chain={1} 
-    className= 'block w-1/2 rounded-md border-1 border-gray-300 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-    unstyled={false} 
-    debug={true}
-    disableSendOnEnter={false}
-    onlyEstimate={false}
-  />
-  );
-
-  // Act: Change the input value
-  const inputElement = getByPlaceholderText('Enter Amount');
-  fireEvent.keyDown(inputElement, { target: { value: '0.001' } });
-
-  // Assert: call function of send
-  expect(inputElement.value).toBe('0.001');
+  expect(inputElement.value).toBe('0.123');
 });
